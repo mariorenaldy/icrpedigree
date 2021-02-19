c32419ccf6900ce0bf92899ba520c81f950fdd40
Riska Dewi <riskadewi@hotmail.com>
2021-02-19 13:27:59 +0700

    Kennel type controller for app

---
+++ application/controllers/app/Kenneltype.php
@@ -0,0 +1,18 @@
+<?php // ARTechnology
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kenneltype extends CI_Controller {
+		public function __construct(){
+			// Call the CI_Controller constructor
+			parent::__construct();
+			$this->load->model('KenneltypeModel');
+		}
+
+		public function get_all(){
+			$res = $this->KenneltypeModel->get_kennel_types()->result();
+			echo json_encode([
+				'status' => true,
+				'data' => $res
+			]);
+		}
+}
