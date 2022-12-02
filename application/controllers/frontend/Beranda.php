d55810c940fd5602c9ced27e146da9f0d480b9cd
Riska Dewi <riskadewi@hotmail.com>
2022-12-02 15:39:56 +0700

    Struktur aplikasi web front end

--- application/controllers/frontend/Beranda.php
+++ application/controllers/frontend/Beranda.php
@@ -8,7 +8,7 @@ class Beranda extends CI_Controller {
 
 	public function index()
 	{
-		$this->data['content'] = 'beranda';
-        $this->load->view("frontend/layout/page_layout", $this->data);
+		$data['content'] = 'beranda';
+		$this->load->view("frontend/layout/page_layout", $data);
 	}
 }
