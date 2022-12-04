aabbb747a61b2e2931bca3e8b08cef059030d633
Mario Renaldy <mariorenaldyy@gmail.com>
2022-12-04 13:24:52 +0700

    Menambahkan controller auth backend

--- application/views/frontend/login.php
+++ application/views/frontend/login.php
@@ -7,17 +7,44 @@
                         <h2 class="fw-bold">Sign In</h2>
                     </header>
 
-                    <div class="d-flex flex-column gap-3" style="margin-top: 5%;">
-                        <input type="text" placeholder="Username">
-                        <input type="password" placeholder="Password">
+                    <!-- <form action="<?= base_url('backend/auth') ?>" method="post" class="form-login d-flex flex-column gap-3" style="margin-top: 5%;">
+                        <input type="text" placeholder="Username" id="username">
+                        <input type="password" placeholder="Password" id="password">
 
                         <div>
                             <button id="btn-masuk" class="btn">Masuk</button>
                             <a href="<?= site_url('frontend/register') ?>" id="btn-daftar" class="btn">Daftar</a>
                         </div>
+                    </form> -->
+
+                    <div class="block-content ">
+                      <form class="form-horizontal form-login" action="<?= base_url('backend/auth') ?>" method="post">
+                        <div class="form-group">
+                          <label class="control-label" for="username">Username</label>
+                          <div class="">
+                            <input class="form-control" type="text" id="username" name="username" placeholder="Masukan username" required>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label class="control-label" for="password">Password</label>
+                          <div class="">
+                            <input class="form-control" type="password" id="password" name="password" placeholder="Masukan password">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="">
+                            <button class="btn btn-md btn-default" type="submit"><i class="si si-login"></i> Masuk</button>
+                            <!-- <a href="#" class="h5" style="float:right;margin-top:9px;">Lupa kata sandi?</a> -->
+                          </div>
+                        </div>
+                      </form>
                     </div>
+
                 </div>
             </div>
         </div>
     </article>
 </main>
+
+<script src="{{base_url()}}assets/sweetalert/dist/sweetalert.min.js"></script>
+<script src="{{base_url()}}assets/oneui/js/pages/login.js"></script>
