235455deaa55b8de69cf81548c4539fe748b15b7
Riska Dewi <riskadewi@hotmail.com>
2022-12-11 12:33:57 +0700

    menu utk back end

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,18 +4,42 @@
          if ($this->session->userdata('use_username')){ ?>
             <nav class="navbar">
                 <div class="container-fluid">
-                    <a href="<?= site_url('backend/Members') ?>" class="text-decoration-none text-reset link-primary">Kelola Member</a>
-                    <a href="<?= site_url('backend/Canines/view_approve') ?>" class="text-decoration-none text-reset link-primary">Kelola Anjing</a>
-                    <a href="<?= site_url('backend/Studs') ?>" class="text-decoration-none text-reset link-primary">Kelola Pacak</a>
-                    <a href="<?= site_url('backend/Births') ?>" class="text-decoration-none text-reset link-primary">Kelola Lahir</a>
-                    <div class="dropdown">
-                        <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           <?php echo $this->session->userdata('use_username'); ?>
-                        </span>
+                     <div class="dropdown">
+                        <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Members</span>
                         <ul class="dropdown-menu">
-                           <li><a class="dropdown-item" href="<?= site_url('backend/Users/logout') ?>">Logout</a></li>
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Members/view_approve') ?>">Approve</a></li>
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Members/') ?>">Manage</a></li>
                         </ul>
-                    </div>
+                     </div>
+                     <div class="dropdown">
+                        <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Canines</span>
+                        <ul class="dropdown-menu">
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Canines/view_approve') ?>">Approve</a></li>
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Canines/') ?>">Manage</a></li>
+                        </ul>
+                     </div>
+                     <div class="dropdown">
+                        <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Studs</span>
+                        <ul class="dropdown-menu">
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Studs/view_approve') ?>">Approve</a></li>
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Studs/') ?>">Manage</a></li>
+                        </ul>
+                     </div>
+                     <div class="dropdown">
+                        <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Births</span>
+                        <ul class="dropdown-menu">
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Births/view_approve') ?>">Approve</a></li>
+                           <li><a class="dropdown-item" href="<?= base_url('backend/Births/') ?>">Manage</a></li>
+                        </ul>
+                     </div>
+                     <div class="dropdown">
+                           <span class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              <?= $this->session->userdata('use_username'); ?>
+                           </span>
+                           <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="<?= base_url('backend/Users/logout') ?>">Logout</a></li>
+                           </ul>
+                     </div>
                 </div>
             </nav>
       <?php } else { ?>
