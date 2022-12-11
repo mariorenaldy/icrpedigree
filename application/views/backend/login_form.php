1a210cc3d95af968528078ab9b4067d155e2e391
Riska Dewi <riskadewi@hotmail.com>
2022-12-11 15:35:17 +0700

    login form styling

--- application/views/backend/login_form.php
+++ application/views/backend/login_form.php
@@ -4,39 +4,36 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
     <div class="container">
         <?php $this->load->view('templates/header'); ?>  
         <div class="row">            
-            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">                          
-                <form class="form-horizontal" action="<?php echo base_url(); ?>backend/Users/validate_login" method="post">
-                    <h3>Login</h3>
+            <div class="col-md-6 col-md-offset-3">  
+                <h3>Login</h3>                        
+                <form class="form-horizontal" align="center" action="<?= base_url(); ?>backend/Users/validate_login" method="post">
                     <div class="text-danger">
                         <?php		
                         if ($this->session->flashdata('login_error')){
                             echo $this->session->flashdata('login_error').'<br/>';
                         }
-                        echo validation_errors();
+                        echo validation_errors().'<br/>';
                         ?>
                     </div>
-
-                    <div class="form-group input-group">
-                        <label class="control-label col-sm-4">Username: </label>
-                        <div class="col-sm-8">
-                            <input class="form-control" type="text" id="username" name="username" placeholder="Nama pengguna" value="<?php echo set_value('username'); ?>">
+                    <div class="input-group mb-3">
+                        <label for="username" class="control-label col-md-2">Username: </label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" id="username" name="username" value="<?= set_value('username'); ?>" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                         </div>
                     </div>
-
-                    <div class="form-group input-group">
-                        <label class="control-label col-sm-4">Password: </label>
-                        <div class="col-sm-8">
+                    <div class="input-group mb-3">
+                        <label for="password" class="control-label col-md-2">Password: </label>
+                        <div class="col-md-6">
                             <input class="form-control" type="password" id="password" name="password" placeholder="Kata Sandi">
                         </div>
                     </div>
-
-                    <div class="form-group input-group">
+                    <div class="input-group mb-3">
                         <button class="btn btn-primary btn-lg" type="submit">Login</button>
                     </div>
                 </form>
@@ -44,7 +41,7 @@
         </div> 
         <?php $this->load->view('templates/footer'); ?>      
     </div>
-<script src="<?php echo base_url(); ?>assets/js/jquery-3.6.1.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery-3.6.1.min.js"></script>
 <script>
     $(document).ready(function(){
         $('#username').focus();
