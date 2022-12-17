0ffb304a25d947fe843bfc002728b576cb496bff
Riska Dewi <riskadewi@hotmail.com>
2022-12-17 14:26:14 +0700

    bug fix dashboard

--- application/controllers/backend/Dashboard.php
+++ application/controllers/backend/Dashboard.php
@@ -2,7 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-		private $navigations;
 		public function __construct(){
 			// Call the CI_Controller constructor
 			parent::__construct();
--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard</title>
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
 <?php
@@ -23,8 +23,8 @@
         </div> 
         <?php $this->load->view('templates/footer'); ?>      
     </div>
-<script src="<?php echo base_url(); ?>assets/js/jquery-3.6.1.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery-3.6.1.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
