7a70c06329ff011c224bdb613b65106fda5cfded
Riska Dewi <riskadewi@hotmail.com>
2022-12-19 12:29:01 +0700

    menampilkan rules di front end

--- application/controllers/frontend/Rules.php
+++ application/controllers/frontend/Rules.php
@@ -1,13 +1,19 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Rules extends CI_Controller {
-    public function __construct()
-    {
-		parent::__construct();
-	}
+		public function __construct(){
+			// Call the CI_Controller constructor
+			parent::__construct();
+			$this->load->model('rulesModel');
+			$this->load->library(array('session'));
+			$this->load->helper(array('url'));
+			$this->load->database();
+		}
 
-	public function index()
-	{
-        $this->load->view("frontend/rules");
-	}
+		public function index(){
+			$where['ru_stat'] = 1;
+			$data['rules'] = $this->rulesModel->get_rules($where)->result();
+			$this->load->view('frontend/rules', $data);
+		}
 }
--- application/views/frontend/rules.php
+++ application/views/frontend/rules.php
@@ -1,62 +1,14 @@
-<!DOCTYPE html>
-<html class="min-vh-100">
-<head>
-    <title>Rules</title>
-    <?php $this->load->view('frontend/layout/head'); ?>
-</head>
-<body class="text-white text-break">
-    <?php $this->load->view('frontend/layout/header'); ?>  
-    <?php $this->load->view('frontend/layout/navbar'); ?>
-
-    <main class="container" id="beranda-main">
-        <article class="row align-items-center justify-content-around">
-            <div class="col-5 mb-5">
-                <header>
-                    <h2 id="article-heading" class="fw-bold">Rules</h2>
-                </header>
-                <p>Lapor pacak:</p>
-                <ul>
-                    <li>foto pacak</li>
-                    <li>foto stb icr bapa dan induk bagian yg ada foto</li>
-                    <li>tanggal pacak cukup 1tanggal saja.</li>
-                    <li>lapor pacak dilakukan oleh pemilik dogi jantan</li>
-                    <li>minimal umur 12bulan untuk jantan dan betina tidak boleh kurang.</li>
-                </ul>
-                
-
-                <p>lapor lahir:</p>
-                <ul>
-                <li>tanggal lahiran anakan.</li>
-                <li>foto menyusui indukan beserta anakan.</li>
-                <li>total anakan dan infokan brp ekor jantan dan betina.</li>
-                <li>foto stb icr bapa dan indukan lagi bagian depan yg ada foto saja.</li>
-                <li>laporan lahir yg wajib lapor adalah pemilik betina.</li>
-                <li>batas waktu lapor lahir adalah 75hari dari tanggal pacak.
-                bila lebih harap HUB ADMIN.</li>
-                </ul>
-                
-
-                <p>PENGAJUAN STAMBUM ANAKAN:</p>
-                <ul>
-                <li>Foto anakan. </li>
-                <li>Jangan di dalam kandang atau di gendong.</li>
-                <li>nama dogi.</li>
-                <li>dob</li>
-                <li>male female</li>
-                <li>nama kennel</li>
-                <li>nama owner</li>
-                <li>alamat owner</li>
-                <li>batas waktu pengajuan adalah 100hari dari tanggal lahir anakan. Bila lewat akan jadi silsilah F1</li>
-                </ul>
-
-                <p>laporan langsung ke email icr_indonesia@yahoo.com <br>
-                Atau ke aplikasi
-                </p>
-            </div>
-        </article>
-    </main>
-    
-    <?php $this->load->view('frontend/layout/footer'); ?>
-</body>
-</html>
-
+<main class="container" id="beranda-main">
+    <article class="row align-items-center justify-content-around">
+        <div class="col-5 mb-5">
+            <header>
+                <h2 id="article-heading" class="fw-bold">Rules</h2>
+            </header>
+            <?php foreach ($rules AS $r){ 
+                echo '<p>'.$r->ru_title.'</p>'; 
+                echo $r->ru_desc; 
+            } ?>    
+            <p>Laporan langsung ke email icr_indonesia@yahoo.com atau ke aplikasi</p>
+        </div>
+    </article>
+</main>
