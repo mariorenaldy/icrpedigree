94668335d64fac53e4af1f11d71955ca2a20e3d7
Riska Dewi <riskadewi@hotmail.com>
2022-12-23 12:53:27 +0700

    Tampilan Birth disesuaikan dgn spek baru

--- application/views/backend/approve_births.php
+++ application/views/backend/approve_births.php
@@ -45,24 +45,24 @@
                 </div>
                 <div class="row">
                     <div class="col-md-3"><b>Foto</b></div>
-                    <div class="col-md-3"><b>Dam</b></div>
-                    <div class="col-md-2"><b>Nama</b></div>
-                    <div class="col-md-2"><b>DOB</b></div>
+                    <div class="col-md-3"><b>DOB</b></div>
+                    <div class="col-md-2" align="center"><b>Jumlah Jantan</b></div>
+                    <div class="col-md-2" align="center"><b>Jumlah Betina</b></div>
                     <div class="col-md-2"></div>
                 </div>
                 <?php foreach ($birth AS $b){ ?>
                     <div class="row">
                         <div class="col-md-3">
-                            <img src="<?= base_url('uploads/canine/'.$b->bir_photo) ?>" class="img-fluid img-thumbnail" alt="canine">
-                        </div>
-                        <div class="col-md-3">
                             <img src="<?= base_url('uploads/births/'.$b->bir_dam_photo) ?>" class="img-fluid img-thumbnail" alt="canine">
                         </div>
-                        <div class="col-md-2">
-                            <?= $b->bir_a_s; ?>
-                        </div>
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <?= $b->bir_date_of_birth; ?>
+                        </div>
+                        <div class="col-md-2" align="right">
+                            <?= $b->bir_male; ?>
+                        </div>
+                        <div class="col-md-2" align="right">
+                            <?= $b->bir_female; ?>
                         </div>
                         <div class="col-md-2">
                             <button type="button" class="btn btn-success" onclick='approve(<?= $b->bir_id ?>, "<?= $b->bir_a_s ?>")'><i class="fa fa-check"></i></button>
