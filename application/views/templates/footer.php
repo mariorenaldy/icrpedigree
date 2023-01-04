6da610a3c4ba5ded893d0337655ca8b85ac515d4
Mario Renaldy <mariorenaldyy@gmail.com>
2023-01-04 14:50:48 +0700

    Memperkecil font size footer frontend dan backend

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,7 @@
    <hr/>
    <div class="row">
       <div class="col-md-4"></div>
-      <div class="col-md-4 text-center">
+      <div class="col-md-4 text-center" style="font-size: 0.8em;">
          Copyright &copy; 2021 <a href="" class="text-decoration-none text-reset"><span class="text-danger">ART</span>echnology.</a><br/>All rights reserved.
       </div>
    </div>
