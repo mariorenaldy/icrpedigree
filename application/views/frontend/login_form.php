e9e951c746f22f1b6800e29679c3e786dbd7e6e0
Mario Renaldy <mariorenaldyy@gmail.com>
2023-01-25 03:10:59 +0700

    Menambahkan iklan di login frontend

--- application/views/frontend/login_form.php
+++ application/views/frontend/login_form.php
@@ -5,62 +5,67 @@
     <?php $this->load->view('frontend/layout/head'); ?>
 </head>
 <body class="text-white text-break">
-    <?php $this->load->view('frontend/layout/header'); ?>  
+    <?php $this->load->view('frontend/layout/header'); ?>
     <?php $this->load->view('frontend/layout/navbar'); ?>
-    <main class="container">
-        <div class="container">
-            <h3 class="text-center text-warning">Login</h3>
-            <div class="row">   
-                <div class="col-md-3"></div>    
-                <div class="col-md-6 text-center">                          
-                    <form class="form-horizontal" action="<?php echo base_url(); ?>frontend/Members/validate_login" method="post">
-                        <div class="text-success">
-                            <?php		
-                                if ($this->session->flashdata('pro')){
+    <div class="d-flex justify-content-between">
+        <div style="width: 40vw;"></div>
+        <main class="container">
+            <div class="container">
+                <h3 class="text-center text-warning">Login</h3>
+                <div class="row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6 text-center">
+                        <form class="form-horizontal" action="<?php echo base_url(); ?>frontend/Members/validate_login" method="post">
+                            <div class="text-success">
+                                <?php
+                                if ($this->session->flashdata('pro')) {
                                     echo 'Register berhasil.<br/>Silakan hubungi ICR admin untuk mendapatkan approval.<br/>';
-                                }
-                                else{
+                                } else {
                                     echo 'Register berhasil.<br/>Gunakan email sebagai username & no. hp sebagai password.<br/>';
                                 }
-                            ?>
-                        </div>
-                        <div class="text-danger">
-                            <?php		
-                            if ($this->session->flashdata('login_error')){
-                                echo $this->session->flashdata('login_error').'<br/>';
-                            }
-                            echo validation_errors();
-                            ?>
-                        </div>
-                        <div class="input-group my-3">
-                            <div class="col-md-1"></div>  
-                            <label for="username" class="control-label col-md-3"><span class="input-group-addon"><i class="fa fa-tag"></i> Username:</span></label>
-                            <div class="col-md-7">
-                                <input class="form-control" type="text" id="username" name="username" placeholder="Nama pengguna" value="<?= set_value('username'); ?>">
+                                ?>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <div class="col-md-1"></div> 
-                            <label for="username" class="control-label col-md-3"><span class="input-group-addon"><i class="fa fa-lock"></i></span> Password:</label>
-                            <div class="col-md-7">
-                                <input class="form-control" type="password" id="password" name="password" placeholder="Kata Sandi">
+                            <div class="text-danger">
+                                <?php
+                                if ($this->session->flashdata('login_error')) {
+                                    echo $this->session->flashdata('login_error') . '<br/>';
+                                }
+                                echo validation_errors();
+                                ?>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <div class="col-md-4"></div>
-                            <div class="col-md-5 text-start">
-                                <button class="btn btn-primary btn-lg" type="submit">Login</button>
-                                <button class="btn btn-danger btn-lg" type="button" onclick="window.location = '<?= base_url() ?>frontend/Members/register'">Daftar</button>
+                            <div class="input-group my-3">
+                                <div class="col-md-1"></div>
+                                <label for="username" class="control-label col-md-3"><span class="input-group-addon"><i class="fa fa-tag"></i> Username:</span></label>
+                                <div class="col-md-7">
+                                    <input class="form-control" type="text" id="username" name="username" placeholder="Nama pengguna" value="<?= set_value('username'); ?>">
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </div>                           
+                            <div class="input-group mb-3">
+                                <div class="col-md-1"></div>
+                                <label for="username" class="control-label col-md-3"><span class="input-group-addon"><i class="fa fa-lock"></i></span> Password:</label>
+                                <div class="col-md-7">
+                                    <input class="form-control" type="password" id="password" name="password" placeholder="Kata Sandi">
+                                </div>
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="col-md-4"></div>
+                                <div class="col-md-5 text-start">
+                                    <button class="btn btn-primary btn-lg" type="submit">Login</button>
+                                    <button class="btn btn-danger btn-lg" type="button" onclick="window.location = '<?= base_url() ?>frontend/Members/register'">Daftar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    </main>
-    <?php $this->load->view('frontend/layout/footer'); ?>      
+        </main>
+        <aside>
+            <img src="<?= base_url('assets/img/ICR_jersey.jpg') ?>" class="img-fluid img-thumbnail" style="width: 40vw;">
+        </aside>
+    </div>
+    <?php $this->load->view('frontend/layout/footer'); ?>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
             $('#username').focus();
         });
     </script>
