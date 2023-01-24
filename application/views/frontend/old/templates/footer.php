c790e278000da286cc370bbe9c90e0a651da86ee
Mario Renaldy <mariorenaldyy@gmail.com>
2023-01-24 14:57:03 +0700

    Hapus app, old, dan file dari repo lama

--- application/views/frontend/old/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="footer">
-   <hr/>
-   <div class="copyright text-center">
-      Copyright &copy; 2021 ICR Pedigree. All rights reserved.
-   </div>
-</div>
--- application/views/frontend/old/templates/header.php
+++
@@ -1,6 +0,0 @@
-<div class="header">
-   <div class="title">
-      <h1>ICR Pedigree</h1>
-   </div>
-   <hr/>
-</div>
