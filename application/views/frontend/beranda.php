738eb97ca5949175bd9aea76c1bcf8a97dfb109d
Mario Renaldy <mariorenaldyy@gmail.com>
2023-02-27 18:05:53 +0700

    Membuat carousel di beranda frontend, mengubah redirect sebelum login ke beranda

--- application/views/frontend/beranda.php
+++ application/views/frontend/beranda.php
@@ -21,6 +21,35 @@
                     ?>
                 </div>
             </div>
+            <div id="adsCarousel" class="carousel slide" data-bs-ride="carousel" style="width: 800px;">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#adsCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#adsCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#adsCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner mb-5">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="<?= base_url('assets/img/brown-dog.svg') ?>" class="d-block m-auto" alt="..." style="height:300px;">
+                    </div>
+                    <div class="carousel-item"  data-bs-interval="10000">
+                        <img src="<?= base_url('assets/img/white-dog.svg') ?>" class="d-block m-auto" alt="..." style="height:300px;">
+                    </div>
+                    <div class="carousel-item"  data-bs-interval="10000">
+                        <video id="carouselVideo" controls muted autoplay class="d-block m-auto" style="height:300px;">
+                            <source src="<?= base_url('assets/video/OneEyedJack.mp4') ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#adsCarousel" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#adsCarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
             <div class="col-sm-6 mb-5">
                 <header>
                     <h2 id="article-heading" class="fw-bold">Pastikan anjing kesayangan anda tersertifikasi</h2>
